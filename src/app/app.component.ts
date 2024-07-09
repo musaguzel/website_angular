5af83e265db1ea68083024c96188307512aba366
musaguzel <musaguzel.7@hotmail.com>
2024-07-09 17:02:09 +0300

    social-buttons , menu-bar , title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tanitim_web';
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+
+    this.items = [
+      {
+        label: '<b>Anasayfa</b>',
+        escape: false,
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            { label: 'Project', url: 'https://www.google.com' },
+            { label: 'Other', command: () => this.someFn() },
+          ]
+        },
+        { label: 'Open' },
+        { label: 'Quit' }
+        ]
+      },
+      {
+        label: 'Kurumsal',
+        icon: 'pi pi-objects-column',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'Projeler',
+        icon: 'pi pi-hammer',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'Medya',
+        icon: 'pi pi-camera',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'Hakkımızda',
+        icon: 'pi pi-align-justify  ',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'İletişim',
+        icon: 'pi pi-phone',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      }
+    ];
+  }
+
+  someFn() {
+    console.log('hi')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
 import { AppComponent } from './app.component';
+import { SocialMediaIconsComponent } from './social-media-icons/social-media-icons.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialMediaIconsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
